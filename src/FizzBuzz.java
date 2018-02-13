da82f8d8b9effd1e239fcbe0641fc9c7462485d7
patrickatcw <pj8392@yahoo.com>
2018-02-13 12:10:11 -0500

    commit intial program structure, directions, and update to gitignore

---
+++ src/FizzBuzz.java
@@ -0,0 +1,21 @@
+/*
+Directions
+Write a “FizzBuzz” program: for the numbers from 1 to 20, if a number is divisible by 3, print out “Fizz”.
+If a number is divisible by 5, print out “Buzz”. If a number is divisible by 3 and 5, print out “Fizz Buzz.”
+Otherwise, just print out the number. Hint: look up the modulo operator.
+Here’s what he first three lines of your program output should look like:
+
+	1
+	2
+	Fizz
+*/
+
+
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+    }
+
+}
