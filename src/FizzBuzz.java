7730e39a0a8ddee4fc9620c121811e31dd2eb20b
patrickatcw <pj8392@yahoo.com>
2018-02-13 13:20:49 -0500

    results

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -16,8 +16,24 @@ public class FizzBuzz {
 
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
+        for (int i = 1; i <= numbers.length; i++) {
 
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+
+            } else if (i % 3==0) {
+                System.out.println("Fizz");
+
+            } else if (i % 5==0) {
+                System.out.println("Buzz");
+
+            } else {
+                System.out.println(String.valueOf(i));
+
+            }
+
+        }
 
     }
-
 }
+
